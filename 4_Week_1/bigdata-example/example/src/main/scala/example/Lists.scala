daa15b2658470685292fffd7d05c035c5de80a86
KlaudiaKromolowska <klaudia.kromolowska@gmail.com>
2022-03-15 10:50:11 +0100

    change folders names

---
+++ 4_Week_1/bigdata-example/example/src/main/scala/example/Lists.scala
@@ -0,0 +1,25 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+name = input() # wczytuje z klawiatury
+print ('Hello World')# wypisze "hello world"
+
+a = 5
+b = 3
+suma = a + b
+b = int(input())  #wczyta liczbe zamiast stringa
+
+print(int(-1.7))  # 
+print(round(-1.7))  # 
+print(math.ceil(1 + 3.8)) # floor(x)
+
+x = int(input())
+if x > 0:
+    print(x)
+else:
+    print(-x)
---
+++ 4_Week_2/stackoverflow/assignment.sbt
@@ -0,0 +1,13 @@
+// Student tasks (i.e. submit, packageSubmission)
+enablePlugins(StudentTasks)
+
+assignmentVersion.withRank(KeyRanks.Invisible) := "2e93aff3"
+datasetUrl := "https://moocs.scala-lang.org/~dockermoocs/bigdata/stackoverflow-grading.csv"
+
+courseraId := ch.epfl.lamp.CourseraId(
+  courseId = "e8VseYIYEeWxQQoymFg8zQ",
+  key = "GxhyluDOSz67hQeqf5pneg",
+  itemId = "ZyW3H",
+  premiumItemId = Some("a9fE2"),
+  partId = "55nJO"
+)
